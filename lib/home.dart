8c835051c56230a902cc78623107957680077cc6
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-12-24 16:30:35 -0800

    add analysis/image and cleanup files

--- lib/home.dart
+++ lib/home.dart
@@ -1,73 +1,71 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  const MyHomePage({Key key}) : super(key: key);
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        body:
-          new Container(
-            child:
-              new Column(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                children: const <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Spectacular Title',
+                      style: TextStyle(
+                          fontSize: 27.0,
+                          color: Color(0xFF000000),
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Roboto'),
+                    ),
                   ),
-    
-                  new Text(
-                  "Spectacular Title",
-                    style: new TextStyle(fontSize:27.0,
-                    color: const Color(0xFF000000),
-                    fontWeight: FontWeight.w700,
-                    fontFamily: "Roboto"),
-                  ),
-    
-                  new Image.asset(
-                    'img/teapot.png',
-                    fit:BoxFit.fitWidth,
-                    )
-                ]
-    
+                ],
               ),
-    
-            padding: const EdgeInsets.all(0.0),
-            alignment: Alignment.center,
-          ),
-    
-        bottomNavigationBar: new BottomNavigationBar(
-          items: [
-            new BottomNavigationBarItem(
-              icon: const Icon(Icons.star),
-              title: new Text('Title'),
-            ),
-    
-            new BottomNavigationBarItem(
-              icon: const Icon(Icons.star),
-              title: new Text('Title'),
-            ),
-    
-            new BottomNavigationBarItem(
-              icon: const Icon(Icons.star),
-              title: new Text('Title'),
-            ),
-    
-            new BottomNavigationBarItem(
-              icon: const Icon(Icons.star),
-              title: new Text('Title'),
-            )
-          ]
-    
+              Flexible(
+                child: FractionallySizedBox(
+                  heightFactor: 0.7,
+                  widthFactor: 0.7,
+                  child: Image.asset(
+                    'assets/images/joshua-coleman-kFRKvJQtNHg-unsplash.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+            ]),
+        padding: const EdgeInsets.all(0.0),
+        alignment: Alignment.center,
+      ),
+      bottomNavigationBar:  BottomNavigationBar(items: [
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.star),
+          title: Text('Title1'),
         ),
-      );
-    }
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.star),
+          title: Text('Title'),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.star),
+          title: Text('Title'),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.star),
+          title: Text('Title'),
+        )
+      ]),
+    );
+  }
 }
