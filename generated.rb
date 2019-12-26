aab9302602f564a10b4732ed609c248a8f77d500
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-12-26 03:13:12 -0800

    yml updates

--- generated.rb
+++ generated.rb
@@ -1,1 +1,6 @@
 linux/* linguist-generated=true
+macos/* linguist-generated=true
+web/* linguist-generated=true
+ios/* linguist-generated=true
+android/* linguist-generated=true
+build/* linguist-generated=true
